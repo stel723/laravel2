a0713733c1f3d5278a38b08cdca3634ea4f50a1e
FedorUltraz <zhfedor33@gmail.com>
2025-06-03 21:21:34 +0300

    meme

---
+++ meme-generator/app/Models/Meme.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Meme extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['top_text', 'bottom_text', 'image_data', 'mime_type'];
+
+    // Добавьте каст для автоматической сериализации
+    protected $casts = [
+        'image_data' => 'binary',
+    ];
+}
